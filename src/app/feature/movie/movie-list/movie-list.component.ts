1bf4cfb066e724b7a7882096dfa92223f9d10622
Emilie Nerl <emilie.nerl1@gmail.com>
2021-01-20 18:57:33 -0500

    added sysSvc to movie-list component

--- src/app/feature/movie/movie-list/movie-list.component.ts
+++ src/app/feature/movie/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from 'src/app/service/movie.service';
+import { SystemService } from 'src/app/service/system.service';
 import { Movie } from '../../../model/movie.class';
 
 @Component({
@@ -11,10 +12,13 @@ export class MovieListComponent implements OnInit {
   title="Movie List";
   movies: Movie[] = [];
 
-  constructor(private movieSvc: MovieService) { }
+  constructor(private movieSvc: MovieService,
+              private sysSvc: SystemService) { }
 
 
   ngOnInit(): void {
+    // if coming from login we should have an authenticated user inside sysSvc
+    console.log('loggedInUser?', this.sysSvc.loggedInUser);
     //populate list of movies
     this.movieSvc.getAll().subscribe(
       resp => {
